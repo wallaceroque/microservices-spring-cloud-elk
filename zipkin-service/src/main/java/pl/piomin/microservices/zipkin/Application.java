532bf8a54a658db71d8cb5d099cf636242329d76
Wallace Roque <wallacerock@gmail.com>
2018-04-18 22:24:41 -0300

    Testes com zipkin, sleuth, stream e rabbitmq

--- zipkin-service/src/main/java/pl/piomin/microservices/zipkin/Application.java
+++ zipkin-service/src/main/java/pl/piomin/microservices/zipkin/Application.java
@@ -2,11 +2,10 @@ package pl.piomin.microservices.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class Application {
 
 	public static void main(String[] args) {
